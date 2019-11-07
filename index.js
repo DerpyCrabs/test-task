b8e7d863e6f94cc161601c455ce6e6b517a6623a
DerpyCrabs <derpycrabs@gmail.com>
2019-11-07 20:05:00 +0700

    Implement backend

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const fetch = require('node-fetch')
+
+const app = express()
+app.use(express.json())
+const port = 3000
+
+async function getExchangeRates() {
+  const data = await fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+  const json = await data.json()
+  return json.Valute
+}
+function priceInRubles(exchangeRates) {
+  return function({ quantity, currency, price }) {
+    if (
+      typeof quantity !== 'number' ||
+      typeof currency !== 'string' ||
+      typeof price !== 'number'
+    ) {
+      throw new Error('Invalid item')
+    }
+    if (currency === 'RUB') {
+      return quantity * price
+    }
+    if (!(currency in exchangeRates)) {
+      throw new Error('Unknown currency')
+    }
+    return quantity * exchangeRates[currency].Value * price
+  }
+}
+function roundCost(cost) {
+  return Math.round(cost * 100) / 100
+}
+app.get('/cart-cost', async (req, res) => {
+  if (req.body === {} || !Array.isArray(req.body['cart'])) {
+    res.status(400).send()
+  }
+  const cart = req.body.cart
+  const exchangeRates = await getExchangeRates().catch(() =>
+    res.status(503).send()
+  )
+
+  try {
+    const cartRubCost = cart
+      .map(priceInRubles(exchangeRates))
+      .reduce((acc, cost) => acc + cost, 0.0)
+    res.send({
+      RUB: roundCost(cartRubCost),
+      EUR: roundCost(cartRubCost / exchangeRates.EUR.Value),
+      USD: roundCost(cartRubCost / exchangeRates.USD.Value)
+    })
+  } catch (e) {
+    res.status(400).send()
+  }
+})
+
+app.listen(port)
